cbe5caa37dac5d456ca3f1c23e776fd859c0031f
Sherif Abdellatif <sharif_pc@hotmail.com>
2022-04-08 10:40:59 +0200

    Events and more props

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,15 @@
 function Todo(props) {
+  function deleteHandler() {
+    console.log("the delete button of "+props.my_custom_attribute_text+" was clicked");
+  }
+
   return (
     <div className="card">
       <h2>{props.my_custom_attribute_text}</h2>
       <div className="actions">
-        <button className="btn">{props.del_text}</button>
+        <button className="btn" onClick={deleteHandler}>
+          {props.del_text}
+        </button>
       </div>
     </div>
   );
